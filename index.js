f557165edee5d2332eb98e69b6b25861a48403ae
cxreiff <cooper@cxreiff.com>
2023-07-05 12:01:41 -0700

    attempt at including assets

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ const url = new URL(document.location.href);
 const params = url.searchParams;
 const id = params.get('id') ?? 1;
 
+import.meta.glob('./crates/sketch_1/assets/**/*', {
+  as: 'raw',
+  eager: true,
+});
+
 import(`./crates/sketch_${id}/pkg/sketch_${id}.js`).then(
-  async ({default: sketch}) => {
+  async ({ default: sketch }) => {
     await sketch().then((s) => {
       s.main_web();
     });
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,18 @@ import ViteRsw from 'vite-plugin-rsw'
 
 export default defineConfig({
   base: "/bevy_sketches/",
+  assetsInclude: [
+    './crates/*/assets/**/*',
+    './crates/*/assets/*',
+    './crates/**/assets/*',
+    './crates/sketch_1/assets/*',
+    './crates/sketch_1/assets/**/*',
+    'crates/sketch_1/assets/**',
+    'crates/sketch_1/assets/**/*',
+    'crates/sketch_1/assets/**/*.png',
+    './crates/sketch_1/assets/**/*.png',
+    '/crates/sketch_1/assets/**/*.png',
+  ],
   plugins: [
     ViteRsw(),
   ],
